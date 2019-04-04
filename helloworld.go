d0e4999d64791925f8a99c13f17e6b9c5f79df26
yang bo <yang.wilby@gmail.com>
2019-04-04 18:20:43 +0200

    use gae datastore, [in]complete key use appengine.Main() to start the server

--- helloworld.go
+++ helloworld.go
@@ -3,52 +3,81 @@ package main
 import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
-	"log"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 	"net/http"
-	"os"
+	"time"
 )
 
+const username = "gordon"
+const pass = "secret!"
+
+type Post struct {
+	Title  string
+	Teaser string
+	Posted time.Time
+	URL    string
+	Tag    string
+}
+
 func main() {
+
 	// http.HandleFunc("/", indexHandler)
 	router := httprouter.New()
 	router.GET("/", index)
 	router.GET("/hello/:name", hello)
-	middleware := NewMiddleware(router, "I'm middleware")
-	//http.Handle("/", router)
-	// [START setting_port]
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+	router.POST("/posts", postPost)
+	router.GET("/protected/", BasicAuth(Protected))
+	http.Handle("/", router)
+	appengine.Main()
+
+}
+func Protected(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Protected!\n")
+}
+
+func BasicAuth(h httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		// Get the Basic Authentication credentials
+		user, password, hasAuth := r.BasicAuth()
+
+		if hasAuth && user == username && password == pass {
+			// Delegate request to the given handle
+			h(w, r, ps)
+		} else {
+			// Request Basic Authentication otherwise
+			w.Header().Set("WWW-Authenticate", "Basic realm=Restricted")
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		}
 	}
-
-	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), middleware))
-}
-
-// The type of our middleware consists of the original handler we want to wrap and a message
-type Middleware struct {
-	next    http.Handler
-	message string
-}
-
-// Make a constructor for our middleware type since its fields are not exported (in lowercase)
-func NewMiddleware(next http.Handler, message string) *Middleware {
-	return &Middleware{next: next, message: message}
-}
-
-// Our middleware handler
-func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// We can modify the request here; for simplicity, we will just log a message
-	log.Printf("msg: %s, Method: %s, URI: %s\n", m.message, r.Method, r.RequestURI)
-	m.next.ServeHTTP(w, r)
-	// We can modify the response here
 }
 
 func index(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
+func postPost(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	ctx := appengine.NewContext(r)
+	post := Post{
+		Title:  r.FormValue("title"),
+		Teaser: r.FormValue("teaser"),
+		Tag:    r.FormValue("tag"),
+		URL:    r.FormValue("url"),
+		Posted: time.Now(),
+	}
+	key := datastore.NewIncompleteKey(ctx, "Post", nil)
+
+	if completeKey, err := datastore.Put(ctx, key, &post); err != nil {
+		log.Errorf(ctx, "datastore.Put: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	} else {
+		log.Errorf(ctx, "delay Call %v", completeKey)
+	}
+
+}
 func hello(w http.ResponseWriter, _ *http.Request, params httprouter.Params) {
 	fmt.Fprint(w, "Hello, world!", params.ByName("name"))
 
